7f8654c8dd094558981186a8f2713f250def12d7
WMXPY <wm@mengw.io>
2017-11-24 23:40:31 -0600

    example

--- src/albacoreLoad.ts
+++ src/albacoreLoad.ts
@@ -1,10 +1,15 @@
 import * as fs from 'fs';
 import Albacore from './albacore';
+import chunk from './chunk';
+import table from './table';
 
 export default function (albacore: Albacore) {
-    let path: string = albacore.getPath();
-    let db: Object = JSON.parse(fs.readFileSync(path + "/albacore.json", {
-        encoding: "utf8"
-    }));
-    albacore.setLink(db);
+    // let path: string = albacore.getPath();
+    // let db: Object = JSON.parse(fs.readFileSync(path + "/albacore.json", {
+    //     encoding: "utf8"
+    // }));
+    // albacore.setLink(db);
+
+
+
 }
